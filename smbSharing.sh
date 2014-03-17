77cae1a91a0ee715b45886c453184a6ad1bee926
Owen Pragel <owen@74bit.com>
2014-03-17 04:07:11 -0700

    Minor SMB sharing extension edits
    Shortened.

--- smbSharing.sh
+++ smbSharing.sh
@@ -3,9 +3,8 @@
 # Tested on 10.8.5
 # Created on March 17th, 2014 by Owen Pragel
 
-smbEnabled=$(defaults read /Library/Preferences/com.apple.filesharingui SMBEnabled)
-smbEnabled=$(echo $smbEnabled| sed 's/ *$//')
-if [ "$smbEnabled" -eq "0" ]; then
+smbEnabled=$(defaults read /Library/Preferences/com.apple.filesharingui SMBEnabled | sed 's/ *$//')
+if [ "$smbEnabled" == "0" ]; then
   echo "<result>False</result>"
 else
   echo "<result>True</result>"
