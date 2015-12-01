cc4977ba4ba3ddff38a39ecf2ddbf7f60d24ac4c
Owen Pragel <owen.pragel@74bit.com>
2015-12-01 01:02:53 -0800

    Updated Firefox extension attribute, minimized

--- Firefox.sh
+++ Firefox.sh
@@ -1,20 +1,12 @@
-#!/bin/sh
-# Reports the Firefox version or not installed.
-# Created by Owen Pragel on August 2nd, 2013
-
-# These can be modified
+#!/bin/bash
 APPLICATION_PATH='/Applications/Firefox.app'
 APPLICATION_VERSION_KEY='CFBundleShortVersionString'
-
-# No need to change below here
-NOT_INSTALLED='N/A'
 
 printf '<result>'
 if [ -d "$APPLICATION_PATH" ]; then
 	version=$(defaults read "$APPLICATION_PATH/Contents/Info.plist" "$APPLICATION_VERSION_KEY")
 	printf "%s" "$version"
 else
-	printf "%s" "$NOT_INSTALLED"
+	printf "N/A"
 fi
 printf '</result>'
-#ea_display_name	Firefox
